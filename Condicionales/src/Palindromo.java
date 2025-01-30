260e5483407a3dfcfe74581c2135d3b6afea8fc3
SantiagoCamacho10 <123046836@upq.edu.mx>
2025-01-30 15:19:32 -0600

    Se crea un programa en el que se le pide al usuario que ingrese una palabra o frase palindroma, y el programa le dira si realmente es palindroma o no

--- Condicionales/src/Palindromo.java
+++ Condicionales/src/Palindromo.java
@@ -4,8 +4,9 @@ public class Palindromo {
         Scanner sc = new Scanner(System.in);
         System.out.println("Dame una palabra o frase: ");
         String palabra = sc.nextLine();
-        String invertida = new StringBuilder(palabra).reverse().toString();
-        if (palabra.equals(invertida)) {
+        String palabra2 = palabra.replaceAll(" ", "");
+        String invertida = new StringBuilder(palabra2).reverse().toString();
+        if (palabra2.equals(invertida)) {
             System.out.println("Palabra o frase palindroma");
         }else{
             System.out.println("Palabra o frase no palindroma");
